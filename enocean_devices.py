4f913e9da025fa3a1ae290ce7897950a9f703410
Antonio Lignan <antonio.lignan@gmail.com>
2019-03-18 21:25:34 +0100

    refactored

--- enocean_devices.py
+++ enocean_devices.py
@@ -21,9 +21,24 @@ DDBB_ADDRESS   = "localhost"
 DDBB_PORT      = 8086
 
 ENOCEAN_DEVICES = {
-    '01:80:F5:BC': 'main_bedroom_temperature',
-    '01:9C:44:11': 'living_room_CO2',
-    '05:10:19:64': 'balcony_temperature'
+    '01:80:F5:BC': {
+        'name': 'main_bedroom_temperature',
+        'func': 0x02,
+        'type': 0x05,
+        'meas': 'TMP'
+    },
+    '01:9C:44:11': {
+        'name': 'living_room_CO2',
+        'func': 0x09,
+        'type': 0x09,
+        'meas': 'CO2'
+    },
+    '05:10:19:64': {
+        'name': 'balcony_temperature',
+        'func': 0x02,
+        'type': 0x13,
+        'meas': 'TMP'
+    }
 }
 
 influxClient = None
@@ -73,6 +88,16 @@ def publish_to_database(values):
             time_precision='ms')
         data = []
 
+# parses and publishes sensor data to the database
+def enocean_parse_and_publish(data, dev):
+    meas = {}
+    packet.select_eep(dev['func'], dev['type'])
+    packet.parse_eep()
+    if dev['meas'] in packet.parsed:
+        meas[dev['name']] = round(packet.parsed[dev['meas']]['value'], 2)
+        logger.info("PUB --> {}: {}".format(dev['name'], meas[dev['name']]))
+        return meas
+
 def main():
     global influxClient, communicator
 
@@ -95,35 +120,12 @@ def main():
             # RORG: 0xA5, FUNC: 0x02, TYPE: 0x05, Manufacturer: 0x2D
             # 01:80:F5:BC->FF:FF:FF:FF (-74 dBm): 0x01 ['0xa5', '0x8', '0x28', '0x2d', '0x80', '0x1',
 
-            meas = {}
-
             if packet.packet_type == PACKET.RADIO:
                 if packet.rorg == RORG.BS4 and packet.sender_hex in ENOCEAN_DEVICES:
-
-                    # a hack as the rorg_type and rorg_func fields are only populated when the learn bit is set,
-                    # perhaps something I don't understand about the protocol... I'm taking the lazy approach...
-                    if ENOCEAN_DEVICES[packet.sender_hex] == 'main_bedroom_temperature':
-                    # if packet.rorg_type == 0x05 and packet.rorg_func == 0x02:
-                        packet.select_eep(0x02, 0x05)
-                        packet.parse_eep()
-                        if 'TMP' in packet.parsed:
-                            meas[ENOCEAN_DEVICES[packet.sender_hex]] = round(packet.parsed['TMP']['value'], 2)
-
-                    if ENOCEAN_DEVICES[packet.sender_hex] == 'balcony_temperature':
-                    # if packet.rorg_type == 0x05 and packet.rorg_func == 0x02:
-                        packet.select_eep(0x02, 0x13)
-                        packet.parse_eep()
-                        if 'TMP' in packet.parsed:
-                            meas[ENOCEAN_DEVICES[packet.sender_hex]] = round(packet.parsed['TMP']['value'], 2)
-
-                    if ENOCEAN_DEVICES[packet.sender_hex] == 'living_room_CO2':
-                    # if packet.rorg_type == 0x09 and packet.rorg_func == 0x09:
-                        packet.select_eep(0x09, 0x09)
-                        packet.parse_eep()
-                        if 'CO2' in packet.parsed:
-                            meas[ENOCEAN_DEVICES[packet.sender_hex]] = round(packet.parsed['CO2']['value'], 2)
-            if meas:
-                publish_to_database(meas)
+                    meas = enocean_parse_and_publish(packet,
+                        ENOCEAN_DEVICES[packet.sender_hex])
+                    if meas is not None:
+                        publish_to_database(meas)
 
         except queue.Empty:
             continue
